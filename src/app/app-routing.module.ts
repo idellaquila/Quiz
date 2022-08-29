1c45f68e1e4f9139519e13d413689e2683d7785f
Massimo <mmanda@sysmanagement.it>
2022-08-29 10:03:48 +0200

    struttura app  e routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Pages/home/home.component';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' }, // redirect alla home
+    { path: '/home', component: HomeComponent }, // rotta alla home
+    // { path: 'second-component', component: SecondComponent },
+    { path: '**', component: HomeComponent }, // pagina per il  404
+  ];
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { QuestionsComponent } from './Pages/questions/questions.component';
+import { ResultsComponent } from './Pages/results/results.component';
+import { CardComponent } from './Components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    QuestionsComponent,
+    ResultsComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
