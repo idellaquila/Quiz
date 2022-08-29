8f3f679aacce4cdb119445a9a0d492bfbf445282
Massimo <mmanda@sysmanagement.it>
2022-08-29 15:01:26 +0200

    varie ed eventuali

--- src/app/Pages/questions/questions.component.ts
+++ src/app/Pages/questions/questions.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { QuizService } from 'src/app/services/quiz.service';
 
 @Component({
@@ -7,9 +8,29 @@ import { QuizService } from 'src/app/services/quiz.service';
   styleUrls: ['./questions.component.css'],
 })
 export class QuestionsComponent implements OnInit {
-  constructor(public quizService: QuizService) {}
+  constructor(public quizService: QuizService, public router: Router) {}
 
+  index: number = 0;
+  TextButton: string = 'Next';
   ngOnInit(): void {
     console.log(this.quizService.selectedCategory.answers);
+  }
+  nextQuestion() {
+    if (this.index < this.quizService.selectedCategory.length - 1) {
+      this.index += 1;
+      console.log('this.quizService.selectedCategory.length');
+      console.log(this.quizService.selectedCategory.length);
+      console.log('this.index');
+      console.log(this.index);
+    }
+    if (this.index == this.quizService.selectedCategory.length - 1) {
+      this.TextButton = 'Show Results';
+      console.log('finito');
+    }
+    if (this.index == this.quizService.selectedCategory.length) {
+      this.router.navigate(['show-results']);
+      console.log('vado verso la rotta!!');
+    }
+    console.log(this.index);
   }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Pages/home/home.component';
 import { QuestionsComponent } from './Pages/questions/questions.component';
+import { ResultsComponent } from './Pages/results/results.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch:'full' }, // redirect alla home
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, // redirect alla home
   { path: 'home', component: HomeComponent }, // rotta alla home
-  { path: 'questions', component: QuestionsComponent },
+  { path: 'questions', component: QuestionsComponent }, // rotta alle domande
+  { path: 'show-results', component: ResultsComponent }, // rotta ai risultati
   { path: '**', component: HomeComponent }, // pagina per il  404
 ];
 
@@ -14,5 +16,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
